488634c7ca5046a08e0a7c3e1c413e1f7f795d9b
Scotty Jirik <ScottyJirik@MacBook-Pro.local>
2023-08-10 08:46:13 -0500

    Added a few new variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var button = document.querySelector(".btn")
+var lowerC = "abcdefghijklmnopqrstuvwxyz"
+var upperC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numberC = "0123456789"
+var specialC = "!#$%&()*+,‑"
 
 // Write password to the #password input
-function writePassword() {
-  var buttonOne = prompt("Please enter the length of your password (Ex. at least 8 characters, no more than 128 characters)")
-
-  if (!buttonOne) {
-    return;
-  }
-
-  if (buttonOne >= 8 && buttonOne <= 128){
-  }
-
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+function writePasswor
 
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);

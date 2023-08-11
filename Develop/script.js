64497b7e3f154c7f7b2d2a4190bb202bc54d7c81
Scotty Jirik <ScottyJirik@MacBook-Pro.local>
2023-08-10 21:56:31 -0500

    Final. TA Chris assisted with loop function to help rendering.

--- Develop/script.js
+++ Develop/script.js
@@ -8,8 +8,9 @@ var specialC = "!#$%&()*+,‑"
 
 // Write password to the #password input
 function writePassword() {
-var password = "";
-var passwordC = "";
+var password = [];
+var passwordC = [];
+var passwordF = [];
 firstClick = prompt("Please enter the length of your password (Ex. at least 8 characters, no more than 128 characters)");
 
 if (firstClick < 8) {
@@ -24,36 +25,59 @@ if (firstClick > 128) {
 var lowerCSelect = confirm("Would you like to use lowercase characters?")
 
 if (lowerCSelect) {
-passwordC += lowerC
+  var lowA = lowerC.split("");
+  passwordC = passwordC.concat(lowA);
+  var ranLowC = getPass(lowA);
+  passwordF.push(ranLowC);
 }
 
 var upperCSelect = confirm("Would you like to use uppercase characters?")
 
 if (upperCSelect) {
-passwordC += upperC
+  var upA = upperC.split("");
+  passwordC = passwordC.concat(upA);
+  var ranUpC = getPass(upA);
+  passwordF.push(ranUpC);
 }
 
 var numberCSelect = confirm("Would you like to use numbers?")
 
 if (numberCSelect) {
-passwordC += numberC
+  var numA = numberC.split("");
+  passwordC = passwordC.concat(numA);
+  var ranNumC = getPass(numA);
+  passwordF.push(ranNumC);
 }
 
 var specialCSelect = confirm("Would you like to use special characters?")
 
 if (specialCSelect) {
-passwordC += specialC
+  var specA = specialC.split("");
+  passwordC = passwordC.concat(specA);
+  var ranSpecC = getPass(specA);
+  passwordF.push(ranSpecC);
 }
 
 console.log(passwordC)
 
-console.log(passwordC.length)
+console.log(passwordF)
 
-for (var i = 0; i < firstClick; i++){
-  password = passwordC[Math.floor(Math.random() * passwordC.length)]
+for (var i = 0; i < firstClick; i++) {
+  var character = getPass(passwordC);
+  password.push(character);
+};  
+for (var i = 0; i < passwordF.length; i++) {
+  password[i] = passwordF[i]
+};
+var passwordFinal = password.join('');
+console.log(passwordFinal);
+document.getElementById("password").value = passwordFinal
 }
 
-document.getElementById("password").value = password
+function getPass(array) {
+  var index = Math.floor(Math.random() * array.length);
+  var element = array[index];
+  return element;
 }
 
 // Add event listener to generate button
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+I am to creat a password generator. TA Chris assisted me with final rendering. Here is the link to the deployed site: https://sjirik.github.io/password-gen/
 # password-gen
